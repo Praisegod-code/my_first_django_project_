baf38c62f484533d8010320ffb80f3c71c7543cb
praisegod-code <brightpraisegod966@gmail.com>
2026-07-17 12:24:52 +0100

    fix build.sh typo, SECRET_KEY, add gitignore

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-set -o erreit
+set -o errexit
 
 #to install dependencies
-pip install -r requirement.txt
+pip install -r requirements.txt
 
 #collect staticfiles
 python manage.py collectstatic --no-input
 
 #to make migrations and migrate
-python manage.py makemigrations && pytho manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
